d4c0b6f6c72ed737c957b8c820b2ca40ff4380ae
Josh <josh@nplan.io>
2020-12-03 15:51:08 +0000

    Overcomplicated solution to learn channels and goroutines

--- day-3/solution.go
+++ day-3/solution.go
@@ -7,6 +7,11 @@ import (
   "strings"
 )
 
+type direction struct {
+  x int
+  y int
+}
+
 func read_file_lines(fileName string) []string {
   fileBytes, err := ioutil.ReadFile(fileName)
   if err != nil {
@@ -17,16 +22,25 @@ func read_file_lines(fileName string) []string {
   return list[:len(list) - 1]
 }
 
-func traverse_slope(slope [][]rune, x_move, y_move int) int {
+func traverse_slope(slope [][]rune, move direction, c chan int) {
   x, y, trees := 0, 0, 0
   for y < len(slope) - 1 {
-    y += y_move
-    x = (x + x_move) % len(slope[y])
+    y += move.y
+    x = (x + move.x) % len(slope[y])
     if slope[y][x] == '#' {
       trees += 1
     }
   }
-  return trees
+  c <- trees
+}
+
+func product(c chan int, num int) int {
+  prod := 1
+  for i := 0; i < num; i++ {
+    val := <-c
+    prod *= val
+  }
+  return prod
 }
 
 func main() {
@@ -35,10 +49,16 @@ func main() {
   for _, line := range file_lines {
     slope = append(slope, []rune(line))
   }
-  route_1 := traverse_slope(slope, 1, 1)
-  route_2 := traverse_slope(slope, 3, 1)
-  route_3 := traverse_slope(slope, 5, 1)
-  route_4 := traverse_slope(slope, 7, 1)
-  route_5 := traverse_slope(slope, 1, 2)
-  fmt.Println("Multiplication of all trees:", route_1 * route_2 * route_3 * route_4 * route_5)
+  moves := []direction{
+    direction{1,1},
+    direction{3,1},
+    direction{5,1},
+    direction{7,1},
+    direction{1,2},
+  }
+  c := make(chan int, len(moves))
+  for _, move := range moves {
+    go traverse_slope(slope, move, c)
+  }
+  fmt.Println("Multiplication of all trees:", product(c, len(moves)))
 }
